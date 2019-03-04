adbfdbe2c4f703a115034a9c9cdcb7cec4fe9067
Cady <candyzhouziqing@hotmail.com>
2019-03-03 19:52:47 -0500

    line_tracking demo final

--- line_tracking/line_tracking.ino
+++ line_tracking/line_tracking.ino
@@ -8,15 +8,18 @@
 Servo servoLeft;
 Servo servoRight;
 
-int threshold = 80;                           // < threshold (0) for white, > threshold (1) for black
-int binary[4] = {0, 0, 0, 0};                 // binary numbers for each sensor read
-int code = 1111;                              // integrated binary read
+int threshold = 100;                           // < threshold (0) for white, > threshold (1) for black
+int binary[4] = {0, 0, 0, 0};                  // binary numbers for each sensor read
+int code = 1111;                               // integrated binary read
+int precode = code;                            // Tslast different binary read
+int was_black  = 0;                           
+int sampling_t = 10;
 
 void setup() {
   tone(4, 3000, 1000);                       // Play tone for 1 second
   delay(1000);                               // Delay to finish tone
 
-  Serial.begin(9600);         
+  Serial.begin(9600
   servoLeft.attach(11);                      // Attach left signal to pin 11
   servoRight.attach(12);                     // Attach right signal to pin 12
 }
@@ -24,21 +27,20 @@ void setup() {
 void loop() {
   convert_binary();   
   
-  Serial.println(binary[0]); 
-  Serial.println(binary[1]); 
-  Serial.println(binary[2]); 
-  Serial.println(binary[3]);
+//  Serial.println(binary[0]); 
+//  Serial.println(binary[1]); 
+//  Serial.println(binary[2]); 
+//  Serial.println(binary[3]);
 
   code = binary[0] * 1000 + binary[1] * 100 + binary[2] * 10 + binary[3];
-  Serial.println(code);
 
-  // test
-  simple_move(1500, 1500);
+  if (code != precode) {
+    Serial.println(code);
+    precode = code;
+  }
 
-  // robot_move(code);
-  delay(50);
-
-  
+  robot_move(code);
+  delay(sampling_t);
 }
 
 /**
@@ -46,38 +48,58 @@ void loop() {
  * Note: 1500, 1500 stops the robot 
  *       1700, 1300 robot moves forward
  */
-void robot_move(code){
+void robot_move(int code){
    switch(code) {
     case 1000:  // pivot left 
-      simple_move(1500, 1300);
+      simple_move(1450, 1600);
       break;
     case 1100:  // moving left 
-      simple_move(1600, 1300);
+      simple_move(1500, 1600);
       break;
     case 1110:  // left corner
-      simple_move(1300, 1300);
+      simple_move(1400, 1650);
+//      delay(sampling_t * 10);
+//      simple_move(1450, 1450);
+//      delay(sampling_t *2);
       break;
-    case 0100:  // slight left 
-      simple_move(1700, 1300);
+    case 100:  // slight left 
+      simple_move(1600, 1600);
       break;
-    case 1100:  // forward 
-      simple_move(1700, 1300);
+    case 110:  // forward 
+      simple_move(1600, 1600);
       break; 
-    case 0010:  // slight right 
-      simple_move(1700, 1300);
+    case 10:  // slight right 
+      simple_move(1600, 1600);
       break;
-    case 0111:  // right corner 
-      simple_move(1700, 1700);
+    case 111:  // right corner 
+      simple_move(1650, 1400);
+//      delay(sampling_t * 10);
+//      simple_move(1450, 1450);
+//      delay(sampling_t *2);
       break;
-    case 0011:  // moving right 
-      simple_move(1700, 1400);
+    case 11:  // moving right 
+      simple_move(1600, 1500);
       break;
-    case 0001:  // right pivot 
-      simple_move(1700, 1500);
+    case 1:  // right pivot 
+      simple_move(1600, 1450);
       break;
     case 1111:  // stop
-       simple_move(1500, 1500);
-       break;
+      if (was_black) {
+        simple_move(1600, 1600);
+        delay(50);
+        was_black = 0;
+      } else {
+        simple_move(1500, 1500);
+        delay(2000);
+        simple_move(1600, 1600);
+        was_black = 1;
+      }
+      break;
+    case 0:
+        simple_move(1450, 1450);
+        delay(100);
+//        Serial.println("All 0s");
+        break;
     default:
         Serial.println("Warning. Check qtis");
         break;
@@ -86,7 +108,7 @@ void robot_move(code){
 
 void simple_move(int left, int right) {
   servoLeft.writeMicroseconds(left);        
-  servoRight.writeMicroseconds(right);
+  servoRight.writeMicroseconds(3000 - right);
 }
 
 /**
@@ -100,17 +122,17 @@ void convert_binary() {
   rawVals[2] = RCTime(lineSensor4);     //Calls funtion 'RCTime' Request reading from QTI sensor at pin 'linesensor1' saves value in variable 'qti'
   rawVals[3] = RCTime(lineSensor5);     //Calls funtion 'RCTime' Request reading from QTI sensor at pin 'linesensor1' saves value in variable 'qti'
   
-  Serial.println(rawVals[0]);  //output for testing purpose
-  Serial.println(rawVals[1]); 
-  Serial.println(rawVals[2]); 
-  Serial.println(rawVals[3]);
+//  Serial.println(rawVals[0]);  //output for testing purpose
+//  Serial.println(rawVals[1]); 
+//  Serial.println(rawVals[2]); 
+//  Serial.println(rawVals[3]);
 
   // convert time to binary numbers
   for (int i = 0; i < 4; i++) {
     if (rawVals[i] > threshold) 
-      binary[i] = 0;
-    else
       binary[i] = 1;
+    else
+      binary[i] = 0;
   }
   
 }
